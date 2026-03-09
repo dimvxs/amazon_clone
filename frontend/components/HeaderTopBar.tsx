e3cb4f6955e6623db183db3a5490d63fbea67176
atomic449 <hailthefoul@gmail.com>
2026-03-09 16:01:18 +0200

    extract header logo into reusable component

--- frontend/components/HeaderTopBar.tsx
+++ frontend/components/HeaderTopBar.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import AllMenuItem from "@/components/AllMenuItem";
 import HeaderActionItem from "@/components/HeaderActionItem";
 import DeliveryInfo from "@/components/DeliveryInfo";
 import SearchBar from "@/components/SearchBar";
+import HeaderLogo from "@/components/HeaderLogo";
 
 export default function HeaderTopBar() {
   return (
@@ -11,10 +11,7 @@ export default function HeaderTopBar() {
       {/* Left section*/}
       <div className="pt-4 layout-sm:pt-0 order-1 flex-1 flex items-center justify-start layout-sm:justify-between gap-3">
         <AllMenuItem mobile />
-        
-        {/* Logo */}
-        <img className="w-[98px] h-[34px] bg-surface-3 flex-shrink-0 mr-3 layout-sm:mr-0" />
-
+        <HeaderLogo />
         <DeliveryInfo />
       </div>
 
@@ -26,8 +23,8 @@ export default function HeaderTopBar() {
       {/* Right section*/}
       <div className="pt-4 layout-sm:pt-0 order-2 layout-sm:order-3 flex-1 flex items-center justify-end ">
         <div className="w-full flex justify-end layout-sm:justify-between items-center gap-3">
-          <HeaderActionItem label="Welcome, sign in" fixedWidth href="/login"/>
-          <HeaderActionItem label="Returns & Orders" hideOnMobile fixedWidth/>
+          <HeaderActionItem label="Welcome, sign in" fixedWidth href="/login" />
+          <HeaderActionItem label="Returns & Orders" hideOnMobile fixedWidth />
           <HeaderActionItem label="Cart" />
         </div>
       </div>
