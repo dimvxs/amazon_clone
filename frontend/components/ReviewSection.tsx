d10955011e09061b39af75a3cd0e828e5d722268
atomic449 <hailthefoul@gmail.com>
2026-03-23 17:55:08 +0200

    add icons to ReviewStatCard component

--- frontend/components/ReviewSection.tsx
+++ frontend/components/ReviewSection.tsx
@@ -4,6 +4,10 @@ import UserReviews from "./UserReviews";
 import WriteReviewCTA from "./WriteReviewCTA";
 import ReviewStatCard from "./ReviewStatCard";
 import RatingBars from "./RatingBars";
+
+import checkCircle from "@/assets/icons/check_circle.svg";
+import thumbUp from "@/assets/icons/thumb_up.svg";
+
 interface Review {
   id: number;
   userName: string;
@@ -19,15 +23,20 @@ interface ReviewSectionProps {
 }
 
 export default function ReviewSection({ reviews }: ReviewSectionProps) {
-  
   return (
     <section className="text-default flex flex-col gap-[21px]">
       <h2 className="text-title-md">Customer reviews</h2>
       <div className="w-full flex flex-col layout-product-sm:flex-row items-stretch justify-between gap-[21px] ">
         <ReviewsRating />
-        <RatingBars/>
-        <ReviewStatCard value={176} label="Verified reviews" />
+        <RatingBars />
         <ReviewStatCard
+          iconSrc={checkCircle}
+          value={176}
+          label="Verified reviews"
+        />
+
+        <ReviewStatCard
+          iconSrc={thumbUp}
           value={176}
           label="Clients recommend this product"
           width="w-[300px]"
--- frontend/components/ReviewStatCard.tsx
+++ frontend/components/ReviewStatCard.tsx
@@ -1,21 +1,23 @@
+import Image from "next/image";
+
 interface StatCardProps {
-  icon?: React.ReactNode; 
+  iconSrc?: string;
   value: string | number;
   label: string;
-  width?: string; 
+  width?: string;
 }
 
 export default function ReviewStatCard({
-  icon,
+  iconSrc,
   value,
   label,
   width = "w-[258px]",
 }: StatCardProps) {
   return (
     <div
-      className={`hidden layout-product-sm:flex flex-col items-center  ${width} py-[16.5px] gap-[18px]`}
+      className={`hidden layout-product-sm:flex flex-col items-center ${width} py-[16.5px] gap-[18px]`}
     >
-      {icon ?? <div className="w-[31px] h-[31px] bg-gray-400"></div>}
+      {iconSrc && <Image src={iconSrc} alt="icon" width={31} height={31} />}
       <p className="text-[24px] leading-[20px] text-center">{value}</p>
       <p className="text-[16px] leading-[20px] text-center">{label}</p>
     </div>
