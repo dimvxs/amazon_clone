5370d7c100f411fc41ef81a9e05e936675fbc673
B0nsen <oitut5@gmail.com>
2026-03-30 19:07:11 +0300

    Implemented saving images to AWS S3 bucket

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4403c0f933317b54da6e2e21283753f42b41b2a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94102739e1830a8f0a24c577752e963a3987efec")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
