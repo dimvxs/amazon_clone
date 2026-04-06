8e3d19f7b5a433a139445a63b47fec19d1175f21
dima <dzhosan777@gmail.com>
2026-04-06 14:55:37 +0200

    add sessions

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -7,7 +7,7 @@ import type { SubmitEventHandler } from "react";
 import { validateLoginForm } from "@/lib/validation/auth";
 
 export default function LogInPage() {
-  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (e) => {
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
 
     const form = e.currentTarget;
@@ -28,19 +28,46 @@ export default function LogInPage() {
       return;
     }
 
-    console.log("Login submitted");
+    // Создаём DTO для отправки на backend
+    const loginDTO = { email, password };
+
+    try {
+      const response = await fetch("http://localhost:5012/api/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginDTO),
+      });
+
+      console.log("Status:", response.status, "OK?", response.ok);
+
+      
+      
+      if (!response.ok) {
+        const message = await response.text();
+        console.error("Login failed:", message);
+        return;
+      }
+
+      const data = await response.text();
+      console.log("Login success:", data);
+      // сделать редирект
+    } catch (err) {
+      console.error("Error connecting to server:", err);
+    }
   };
 
   return (
-    <main className="flex py-[141px] bg-surface-1 items-center justify-center">
-      <AuthCard title="Log in" buttonText="Log in" onSubmit={handleSubmit}>
-        <AuthInput placeholder="Email" type="text" name="email" />
-        <AuthInput placeholder="Password" type="password" name="password" />
-        <AuthCheckbox
-          name="terms"
-          label="I agree with Terms and Service and Privacy Policy"
-        />
-      </AuthCard>
-    </main>
+      <main className="flex py-[141px] bg-surface-1 items-center justify-center">
+        <AuthCard title="Log in" buttonText="Log in" onSubmit={handleSubmit}>
+          <AuthInput placeholder="Email" type="text" name="email" />
+          <AuthInput placeholder="Password" type="password" name="password" />
+          <AuthCheckbox
+              name="terms"
+              label="I agree with Terms and Service and Privacy Policy"
+          />
+        </AuthCard>
+      </main>
   );
 }
