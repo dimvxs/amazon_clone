fa807edda82329af67c704c526caaff160cb4e2d
MihailFasteks <143994403+MihailFasteks@users.noreply.github.com>
2026-04-11 14:33:42 +0300

    adapt SalesBanner

--- frontend/components/SalesBanner.tsx
+++ frontend/components/SalesBanner.tsx
@@ -3,19 +3,28 @@ import Image from 'next/image';
 interface Banner {
   title: string;
   imageSrc: string;
-  href: string;
+  href?: string;
 }
 
 export default function SalesBanner({ title, imageSrc, href = '#' }: Banner) {
-    return (
-      <div className="w-[1528px] h-[281px] rounded-[15px] border border-[#2F3A52] shrink-0 py-[15px] px-[20px] gap-[10px]">
-           <Image
-            src={imageSrc}
-            alt={`Category ${title}`}
-            width={1528}
-            height={281}
-            className="object-cover w-full h-full"
-          />     
+  return (
+  
+    <div className="w-full max-w-[1528px] mx-auto px-4 md:px-0">
+      
+      <div className="
+        relative w-full overflow-hidden rounded-[15px] border border-[#2F3A52] shrink-0
+        h-[281px] 
+        max-md:h-[281px]
+      ">
+        <Image
+          src={imageSrc}
+          alt={title}
+          fill
+          className="object-cover"
+          sizes="(max-width: 1528px) 100vw, 1528px"
+          priority
+        />
       </div>
-    );
-  }
+    </div>
+  );
+}
