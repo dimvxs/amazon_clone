9efd623c2a973dc28cae9dce883f5a4dd339021d
MihailFasteks <143994403+MihailFasteks@users.noreply.github.com>
2026-04-13 15:05:45 +0300

    adapt mobile fixed heigth mainpage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,8 +8,10 @@ import CatalogSlider from "@/components/CatalogSlider";
 
 export default function Home() {
   return (
-    /* Внешний слой: Темный фон на всю высоту, центрирует содержимое */
+   
     <div className="relative min-h-screen bg-[#131921] flex flex-col items-center font-sans overflow-x-hidden" style={{ backgroundColor: '#070913' }}>
+      
+    
       <div className="absolute top-0 left-0 w-full h-[1024px] z-[1] pointer-events-none">
         <Image
           src="/images-temp/bg-stars.png"
@@ -17,11 +19,7 @@ export default function Home() {
           fill
           className="object-cover"
           priority
-          
-          
         />
-        
-        
         <div 
           className="absolute bottom-0 left-0 w-full h-[400px] z-[2]" 
           style={{ 
@@ -29,25 +27,31 @@ export default function Home() {
           }} 
         />
       </div>
+
+      {/* Контентный слой */}
       <div className="relative z-10 w-full flex flex-col items-center pt-[40px] gap-[40px]">
         <RecommendRow1 />
+        
         <BestSellersBanner
-        title="Best Sellers in Grocery & Gourmet Food"
-        imageSrc="/images-temp/best_sellers.jpg" 
-        href="#" />
+          title="Best Sellers in Grocery & Gourmet Food"
+          imageSrc="/images-temp/best_sellers.jpg" 
+          href="#" 
+        />
+        
         <RecommendRow2 />
+        
         <CatalogSlider/>
+        
         <SalesBanner
-        title="banner"
-        imageSrc="/images-temp/sales.jpg" 
-        href="#" />
+          title="banner"
+          imageSrc="/images-temp/sales.jpg" 
+          href="#" 
+        />
+        
         <RecommendRow3 />
         
-        {/* Запас места снизу, чтобы проверить, как фото уходит вверх при скролле */}
-        <div className="h-[1000px] w-full" />
+        <div className="pb-[80px]" />
       </div>
-     
-      </div>
-   
+    </div>
   );
 }
