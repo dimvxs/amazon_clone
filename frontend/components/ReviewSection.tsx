e291887f5658c2198528b2398460bfa28647a13e
atomic449 <hailthefoul@gmail.com>
2026-04-18 16:47:46 +0300

    require login before writing a review

--- frontend/components/ReviewSection.tsx
+++ frontend/components/ReviewSection.tsx
@@ -4,11 +4,11 @@ import UserReviews from "./UserReviews";
 import WriteReviewCTA from "./WriteReviewCTA";
 import ReviewStatCard from "./ReviewStatCard";
 import RatingBars from "./RatingBars";
-
+import ReviewModal from "./ReviewModal";
 import checkCircle from "@/assets/icons/check_circle.svg";
 import thumbUp from "@/assets/icons/thumb_up.svg";
-
-
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 interface Review {
   id: number;
   userName: string;
@@ -19,7 +19,6 @@ interface Review {
   helpfulCount: number;
   images: string[];
 }
-
 interface ReviewSectionProps {
   reviews: Review[];
   product: any;
@@ -31,16 +30,12 @@ interface ReviewSectionProps {
     clientsRecommend: number;
   };
 }
-import { useState } from "react";
-import ReviewModal from "./ReviewModal";
-
 export default function ReviewSection({
   reviews,
   product,
   reviewStats,
 }: ReviewSectionProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
   const {
     averageRating,
     ratingCount,
@@ -48,7 +43,25 @@ export default function ReviewSection({
     verifiedCount,
     clientsRecommend,
   } = reviewStats;
-
+  const router = useRouter();
+  const handleWriteReview = async () => {
+    try {
+      const check = await fetch("http://localhost:5012/api/user/islogin", {
+        method: "GET",
+        credentials: "include",
+      });
+      const isLoggedIn = await check.json();
+      console.log("Is logged in:", isLoggedIn);
+      if (isLoggedIn) {
+        setIsModalOpen(true);
+      } else {
+        router.push("/login");
+      }
+    } catch (err) {
+      console.error("Error:", err);
+      router.push("/login");
+    }
+  };
   return (
     <section className="flex flex-col gap-[42px] justify-center items-center">
       <h2 className="text-title-md self-start">Customer reviews</h2>
@@ -70,7 +83,7 @@ export default function ReviewSection({
           label="Clients recommend this product"
           width="w-[300px]"
         />
-        <WriteReviewCTA onClick={() => setIsModalOpen(true)} />
+        <WriteReviewCTA onClick={handleWriteReview} />
       </div>
 
       <div className="flex flex-col max-w-[1076px] gap-[21px]">
