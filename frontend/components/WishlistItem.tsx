7e1d8b31736f577783d0c9d1b438c2acfb0ac8aa
atomic449 <hailthefoul@gmail.com>
2026-04-20 19:23:12 +0300

    update WishlistItem

--- frontend/components/WishlistItem.tsx
+++ frontend/components/WishlistItem.tsx
@@ -29,23 +29,35 @@ export default function WishlistItem({
       <div className="w-[87px] h-[87px] relative rounded-[12px] overflow-hidden shrink-0">
         <Image src={imageSrc} alt="product" fill />
       </div>
-      <div className="flex flex-col max-w-[700px] gap-[4px]">
-        <span className=" font-medium text-[16px] leading-[20px">{title}</span>
-        <span className="flex items-center text-[12px] leading-[16px] gap-[3px]">
-          <span className="text-main/50">{rating}</span>
-          <StarsRating size={13} rating={rating} />
-        </span>
-      </div>
-      <div className="flex flex-col ml-auto items-end gap-[8px]">
-        <DeleteButton onClick={onDelete} />
-        <span className="text-[20px] leading-[100%] text-right">{price}</span>
-        <button
-          className="whitespace-nowrap text-[14px] leading-[20px] text-center align-middle 
-          h-[30px] px-[22.5px] bg-surface-accent rounded-[20px]"
-          onClick={onAddToCart}
-        >
-          Add to cart
-        </button>
+      <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full h-fit gap-[4px] ">
+          <div className="flex justify-between">
+            <span className=" font-medium text-[16px] leading-[20px">
+              {title}
+            </span>
+            <DeleteButton onClick={onDelete} />
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row sm:items-start">
+          <span className="flex items-start text-[12px] leading-[16px] gap-[3px]">
+            <span className="text-main/50">{rating}</span>
+            <StarsRating size={13} rating={rating} />
+          </span>
+
+          <div className="flex flex-col mt-[8px] sm:mt-0 sm:ml-auto sm:items-end gap-[8px]">
+            <span className="text-[20px] leading-[100%] sm:text-right">
+              {price}
+            </span>
+
+            <button
+              className="whitespace-nowrap text-[14px] leading-[20px] text-center align-middle 
+              h-[30px] px-[22.5px] bg-surface-accent rounded-[20px]"
+              onClick={onAddToCart}
+            >
+              Add to cart
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
