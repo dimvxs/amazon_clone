f5325d70811597e415f7e72952e427684c31e84d
atomic449 <hailthefoul@gmail.com>
2026-04-25 15:39:26 +0300

    revert user fetcher to api

--- frontend/lib/api/user.ts
+++ frontend/lib/api/user.ts
@@ -1,4 +1,4 @@
-export const USER_KEY = "/data/account-details.json";
+export const USER_KEY = "/http://localhost:5012/api/user/account/";
 ///data/account-details.json
 //http://localhost:5012/api/user/account/
 export const fetcher = async (url: string) => {
