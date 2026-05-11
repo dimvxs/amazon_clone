5a5016b339a7be6105f01a25190db48f60e51ec3
dima <dzhosan777@gmail.com>
2026-05-11 13:42:59 +0300

    fixed automapper

--- backend/BLL/Profiles/MappingProfile.cs
+++ backend/BLL/Profiles/MappingProfile.cs
@@ -15,10 +15,33 @@ namespace backend.BLL.Profiles
             CreateMap<CreditCardDTO, CreditCard>().ReverseMap();
             CreateMap<FilterDTO, Filter>().ReverseMap();
             CreateMap<FilterValueDTO, FilterValue>().ReverseMap();
-            CreateMap<OrderDTO, Order>().ReverseMap();
+
+            CreateMap<OrderDTO, Order>();
+            CreateMap<Order, OrderDTO>();
+
+            CreateMap<OrderItemDTO, OrderItem>();
+            CreateMap<OrderItem, OrderItemDTO>()
+                .ForMember(
+                    dest => dest.ProductName,
+                    opt => opt.MapFrom(src => src.Product.Name)
+                )
+                .ForMember(
+                    dest => dest.ProductPrice,
+                    opt => opt.MapFrom(src => src.Product.Price)
+                )
+                .ForMember(
+                    dest => dest.ProductImageUrl,
+                    opt => opt.MapFrom(src => src.Product.Images
+                        .OrderByDescending(img => img.IsMain)
+                        .ThenBy(img => img.SortOrder)
+                        .Select(img => img.ImageUrl)
+                        .FirstOrDefault()
+                    )
+                );
+
             CreateMap<ProductImageCreateDTO, ProductImageDTO>()
-            .ForMember(dest => dest.ImageUrl, opt => opt.Ignore());
-            CreateMap<OrderItemDTO, OrderItem>().ReverseMap();
+                .ForMember(dest => dest.ImageUrl, opt => opt.Ignore());
+
             CreateMap<ProductDTO, Product>().ReverseMap();
             CreateMap<ProductImageDTO, ProductImage>().ReverseMap();
             CreateMap<ReviewDTO, Review>().ReverseMap();
@@ -26,7 +49,7 @@ namespace backend.BLL.Profiles
             CreateMap<UserDTO, User>().ReverseMap();
             CreateMap<WishlistDTO, Wishlist>().ReverseMap();
             CreateMap<ReviewImagesDTO, ReviewImagesDTO>().ReverseMap();
-            CreateMap<ProductMetadata,ProductMetadataDTO>().ReverseMap();
+            CreateMap<ProductMetadata, ProductMetadataDTO>().ReverseMap();
         }
     }
 }
