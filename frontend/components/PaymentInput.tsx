8ca55ed0aeddd73bbbe9bb9754c7151c32d3e57e
atomic449 <hailthefoul@gmail.com>
2026-05-19 19:31:13 +0300

    add required validation for payment form

--- frontend/components/PaymentInput.tsx
+++ frontend/components/PaymentInput.tsx
@@ -3,27 +3,35 @@
 import { FormInput } from "@/components/FormInput";
 import { InputWrapper } from "@/components/InputWrapper";
 import type { ComponentType, SVGProps } from "react";
+import { FieldError } from "react-hook-form";
+import { FormError } from "./FormError";
 
 type PaymentInputProps = {
   label: string;
-  name: string;
   placeholder?: string;
-  defaultValue?: string;
   Icon: ComponentType<SVGProps<SVGSVGElement>>;
-};
+  error?: FieldError;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
 
 export function PaymentInput({
   label,
   placeholder,
-  name,
   Icon,
+  error,
+  ...props
 }: PaymentInputProps) {
   return (
     <InputWrapper label={label} className="w-full">
       <div className="w-full h-[40px] bg-input-surface-default flex items-center rounded-[10px] px-[16px] gap-[6px]">
         <Icon className="shrink-0 text-input" />
-        <FormInput name={name} placeholder={placeholder} className="!p-0 w-full" />
+         <FormInput
+          placeholder={placeholder}
+          className="!p-0 w-full"
+          {...props}
+        />
       </div>
+       <FormError message={error?.message} />
     </InputWrapper>
   );
 }
