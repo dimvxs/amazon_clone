5f5ade60171fe48304b02641dce1cfced4a28d15
atomic449 <hailthefoul@gmail.com>
2026-06-11 15:17:10 +0300

    fix alt bg image

--- frontend/app/(bg-alt)/layout.tsx
+++ frontend/app/(bg-alt)/layout.tsx
@@ -1,7 +1,7 @@
 export default function AltLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="absolute inset-0 bg-[url('/images/homepage/bg-stars.png')] bg-repeat-y bg-[length:100%_auto] opacity-30" />
+      <div className="absolute inset-0 bg-[url('/images/homepage/bg.jpg')] bg-repeat-y bg-[length:100%_auto] opacity-30" />
       <div className="relative z-10">{children}</div>
     </>
   );
