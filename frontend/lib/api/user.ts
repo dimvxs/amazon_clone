99a296d6315fcb25c811735b2c01a727589f0b15
atomic449 <hailthefoul@gmail.com>
2026-06-15 19:58:53 +0300

    update user api call

--- frontend/lib/api/user.ts
+++ frontend/lib/api/user.ts
@@ -1,6 +1,5 @@
-export const USER_KEY = "http://localhost:5012/api/user/account/";
-///data/account-details.json
-//http://localhost:5012/api/user/account/
+export const USER_KEY = `${process.env.NEXT_PUBLIC_API_URL}/api/user/account/`;
+
 export const fetcher = async (url: string) => {
     const res = await fetch(url, { credentials: 'include' });
     const data = await res.json();
